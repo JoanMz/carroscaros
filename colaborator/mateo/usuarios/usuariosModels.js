c17a0aeaa971b61abd25d18f0a199ea9d3e99b85
vagrant <vagrant@servidorUbuntu>
2025-03-20 23:01:53 +0000

    createUser module

--- colaborator/mateo/usuarios/usuariosModels.js
+++ colaborator/mateo/usuarios/usuariosModels.js
@@ -12,3 +12,13 @@ const connection = mysql.createPool({
 });
 
 // AQUI CREO LOS MODELOS QUE ME TOCAN EJ MODELO CREAR USUARIO MODELO CREAR USUARIO ADMIN
+
+
+async function createUser(name, email, date_birth, password){
+    const result = await connection.query('INSERT INTO users(name, email, date_birth, password) VALUES(?,?,?,?)', [name, email, date_birth, password]);
+    return result;
+}
+
+module.exports = {
+    createUser
+};
