cb4e2f2fc97dbc053255e733193a2549fcf899d6
vagrant <vagrant@servidorUbuntu>
2025-03-20 22:49:22 +0000

    createPoolusuariosModel

---
+++ src/usuarios/models/usuariosModels.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+require('mysql2/promise');
+
+
+const connection = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    port: process.env.MYSQL_PORT,
+    maxIdle: 2
+});
+
+//async function traerUsuarios
